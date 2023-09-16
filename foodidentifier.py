d2ca34c4f57e82c5d5db7cc783f7b464ff470f8b
Alex Kranias <alexander.kranias@gatech.edu>
2023-09-16 15:42:37 -0400

    YEAST food

--- foodidentifier.py
+++ foodidentifier.py
@@ -1,11 +1,14 @@
 import requests
-img = <'replace-with-path-to-image'>
-api_user_token = <'replace-with-your-api-user-token'>
+img = 'C:\\Users\\Alex Kranias\\Bite\\images_test\\IMG_4904.JPG'
+api_user_token = '72858ab56101a40750278b25356e2ddf8adce260'
 headers = {'Authorization': 'Bearer ' + api_user_token}
 
 # Single/Several Dishes Detection
 url = 'https://api.logmeal.es/v2/image/segmentation/complete'
 resp = requests.post(url,files={'image': open(img, 'rb')},headers=headers)
+
+print(resp.status_code)
+print(resp.json()['imageId'])
 
 # Nutritional information
 url = 'https://api.logmeal.es/v2/recipe/nutritionalInfo'
